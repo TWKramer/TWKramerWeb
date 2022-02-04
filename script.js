5ff92e6f3ba8b27c7a345c9b8b7834499d3d16db
DawgLb <tkramerwfs@gmail.com>
2022-02-04 02:30:07 -0600

    transfering from mac to pc

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var dt = new Date();
                 document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
 
+                
+
 $(".card").click(function(){
     window.location=$(this).find("card-content3").attr("href"); 
     return false;
     });
+
 
 
 
